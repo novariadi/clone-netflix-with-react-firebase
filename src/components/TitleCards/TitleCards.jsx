026f0d47ce5d37706a9d84bd7407793f974c74e1
novariadi <mnovariadi@gmail.com>
2024-09-11 16:02:05 +0700

    generate movie from api tmdb

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,10 +1,19 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import './TitleCards.css';
 import cards_data from "../../assets/cards/Cards_data"
 
 
-const TitleCards = ({title, category}) => {
+const TitleCards = ({ title, category }) => {
+    const [apiData, setApiData] = useState([]);
     const cardsRef = useRef();
+
+    const options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2YTA4ODc2NzYxZDY2MzQwMDRmZjVhYzk2NTc5N2E3YyIsIm5iZiI6MTcyNjA0NDExNi43NzI3MTksInN1YiI6IjY2ZTE1NmFhMzk2ODYxOWVmODA1MTViYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.q8hYphDnUVOxdf_9M-Yuj8SFrcdes9fzcXMKpnZg0Fg'
+        }
+    };
 
     const handleWheel = (event) => {
         event.preventDefault();
@@ -12,16 +21,21 @@ const TitleCards = ({title, category}) => {
     }
 
     useEffect(() => {
+        fetch(`https://api.themoviedb.org/3/movie/${category?category : 'now_playing'}?language=en-US&page=1`, options)
+            .then(response => response.json())
+            .then(response => setApiData(response.results))
+            .catch(err => console.error(err));
+        
         cardsRef.current.addEventListener('wheel', handleWheel);
     },[])
     return (
         <div className="titlecards">
             <h2>{title?title:'Popular on Netflix'}</h2>
             <div className="card-list" ref={cardsRef}>
-                {cards_data.map((card, index) => {
+                {apiData.map((card, index) => {
                     return <div className="card" key={index}>
-                        <img src={card.image} alt="" />
-                        <p>{card.name}</p>
+                        <img src={`https://image.tmdb.org/t/p/w500/${card.backdrop_path}`} alt="" />
+                        <p>{card.original_title}</p>
                     </div>
                 })}
             </div>
