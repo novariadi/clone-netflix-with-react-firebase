9b3758d5c263bfb841aa5df52c6e201f4d790b20
novariadi <mnovariadi@gmail.com>
2024-09-11 12:44:39 +0700

    buat struktur page

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './Home.css';
+import Navbar from "../../components/Navbar";
+
+
+const Home = () => {
+    return (
+        <div className="home">
+            <Navbar />
+            <img src="" alt="" />
+        </div>
+    )
+}
+
+export default Home;
