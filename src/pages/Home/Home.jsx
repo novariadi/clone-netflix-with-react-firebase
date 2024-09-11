22658591c8335e3d1861a2f2661e81d43ee79db4
novariadi <mnovariadi@gmail.com>
2024-09-11 16:42:15 +0700

    crate dark backgroud navbar when vertical scroll and transparent when top

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import './Home.css';
-import Navbar from "../../components/Navbar";
+import Navbar from "../../components/Navbar/Navbar";
 import hero_banner from '../../assets/hero_banner.jpg';
 import hero_title from '../../assets/hero_title.png';
 import play_icon from '../../assets/play_icon.png';
